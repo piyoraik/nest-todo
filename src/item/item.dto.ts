ec61edbadfa9ecd7e340616527c6a7414b5faac4
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2020-12-22 22:08:29 +0900

    CRUDのロジックを記述

---
+++ src/item/item.dto.ts
@@ -0,0 +1,15 @@
+import { isNotEmpty, IsNotEmpty, isString, IsString} from "class-validator";
+
+export class CreateItemDTO {
+  @IsNotEmpty()
+  @IsString()
+  todo: string;
+
+  @IsNotEmpty()
+  @IsString()
+  limit: string;
+
+  @IsNotEmpty()
+  @IsString()
+  deletePassword: string;
+}
--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
-
+import { Inject, Injectable } from '@nestjs/common';
+import { Item } from "src/entities/item.entity";
+import { DeleteResult, InsertResult, Repository, UpdateResult } from "typeorm";
+import { InjectRepository } from "@nestjs/typeorm";
+import { CreateItemDTO } from "./item.dto";
 @Injectable()
-export class ItemService {}
+export class ItemService {
+  constructor(
+    @InjectRepository(Item)
+    private readonly itemRepository: Repository<Item>
+  ) { }
+  
+  // テーブル全データを取得する関数
+  async findAll(): Promise<Item[]> {
+    return await this.itemRepository.find();
+  }
+
+  // テーブルにレコードを追加する関数
+  async create(item: CreateItemDTO): Promise<InsertResult> {
+    return await this.itemRepository.insert(item);
+  }
+
+  //idを指定してテーブルから取得
+  async find(id: number): Promise<Item> | null {
+    return await this.itemRepository.findOne({ id: id });
+  }
+
+  //idを指定してテーブルのデータを更新
+  async update(id: number, item): Promise<UpdateResult> {
+    return await this.itemRepository.update(id, item);
+  }
+
+  //idを指定してレコードを削除
+  async delete(id: number): Promise<DeleteResult> {
+    return await this.itemRepository.delete(id);
+  }
+}
